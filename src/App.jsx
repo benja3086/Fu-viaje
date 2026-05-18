a66907c8a908d0ce592e4bd2e604b5731cd4511d
benja3086 <benjaminquinteros785@gmail.com>
2026-05-18 17:02:41 -0300

    Primer commit proyecto Fu-viaje

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import React from "react";
 import Home from "./Home/Home.jsx";
-import Creacion from "./sobre/Creacion.jsx";
-import SobreNosotros from "./sobre/Nosotros.jsx";
-import Carousel from "./sobre/carrousel.jsx";
-import Proposito from "./Proposito/Proposito.jsx"; 
 import './App.css';
 import Contacto from "./Contacto/Contacto.jsx";
 function App() {
   return (
     <main>
       <Home />
-      <Proposito />
-      <Carousel/>
-      <SobreNosotros />
       <Contacto />
     </main>
   );
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,16 +3,12 @@ import "./Home.css";
 function Home() {
   return (
     <header>
-      <h1>The Kingdom </h1>
-      <nav className="menu">
-        <a href="#nosotros" className="sobremi">
-          Nosotros
-        </a>
-        <a href="#creaciones" className="sobremi">
-          Creaciones
-        </a>
-      </nav>
-    </header>
+   <h2>Proyecto puente</h2>
+      <h1>Llevando el 100% de la iglesia donde Dios quiere</h1>
+      <h5>Apoyando la mision al Pueblo Vasco</h5>    
+      <button>Apoyar la Mision</button>
+      <img src="https://imgs.search.brave.com/fNahUWg_3h7Hb9W9DPVTSblTmnBcVdgqTp6NDnOOn4Q/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMubW5zdGF0aWMu/Y29tL2Q0Lzg4L2Q0/ODgwYmYwNjNkODRi/ZDE0YzE0Y2Q2MjVl/ZTQwODY5LmpwZw" alt="" />
+      </header>
   );
 }
 
